bb33a6df4d4a6bce0c10dd82c0e44dec5da06411
Andrei Balzan <andreibalzan@gmail.com>
2022-10-20 20:09:41 +0100

    implementing Bcrypt

--- src/main/java/com/andrei/bankapp/controller/LoginController.java
+++ src/main/java/com/andrei/bankapp/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.andrei.bankapp.model.Customer;
 import com.andrei.bankapp.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,13 @@ public class LoginController {
 
     private final CustomerRepository customerRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         try {
+            String hashPassword = passwordEncoder.encode(customer.getPassword());
+            customer.setPassword(hashPassword);
             Customer savedCustomer = customerRepository.save(customer);
             return ResponseEntity.ok("User registered successfully" + savedCustomer.getEmail());
         } catch (Exception e) {
