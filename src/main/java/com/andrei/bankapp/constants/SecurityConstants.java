01468826cd30ac380f80e3aff01b600bd8fb3ba4
Andrei Balzan <andreibalzan@gmail.com>
2022-11-24 08:58:35 +0000

    updating secret to meet requirements and commenting debug logs options

--- src/main/java/com/andrei/bankapp/constants/SecurityConstants.java
+++ src/main/java/com/andrei/bankapp/constants/SecurityConstants.java
@@ -2,6 +2,6 @@ package com.andrei.bankapp.constants;
 
 public interface SecurityConstants {
 
-    String JWT_KEY = "secret";
+    String JWT_KEY = "OUIyMzE3QzJDOTQxQTE3OTEzMEQwRDI4OTYxQUI1NDJDODg3NDU2NThCRTMyOEY1NTc0MjJFQTBBRjhGNjBFOA";
     String JWT_HEADER = "Authorization";
 }
