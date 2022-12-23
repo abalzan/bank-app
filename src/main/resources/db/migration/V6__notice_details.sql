04184f289ca7b464da3f7dc6664d9b668eee3244
Andrei Balzan <andreibalzan@gmail.com>
2022-12-23 13:45:40 +0000

    updating sql script, for the notices to be visible for a long time

--- src/main/resources/db/migration/V6__notice_details.sql
+++ src/main/resources/db/migration/V6__notice_details.sql
@@ -13,24 +13,24 @@ CREATE TABLE `notice_details` (
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('Home Loan Interest rates reduced', 'Home loan interest rates are reduced as per the goverment guidelines. The updated rates will be effective immediately',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('Net Banking Offers', 'Customers who will opt for Internet banking while opening a saving account will get a $50 amazon voucher',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('Mobile App Downtime', 'The mobile application of the EazyBank will be down from 2AM-5AM on 12/05/2020 due to maintenance activities',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('E Auction notice', 'There will be a e-auction on 12/08/2020 on the Bank website for all the stubborn arrears.Interested parties can participate in the e-auction',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('Launch of Millennia Cards', 'Millennia Credit Cards are launched for the premium customers of EazyBank. With these cards, you will get 5% cashback for each purchase',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
 
 INSERT INTO `notice_details` (`notice_summary`, `notice_details`, `notice_beg_date`, `notice_end_date`, `created_date`, `updated_date`)
 VALUES ('COVID-19 Insurance', 'EazyBank launched an insurance policy which will cover COVID-19 expenses. Please reach out to the branch for more details',
-        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30 DAY, SYSDATE(), null);
+        SYSDATE() - INTERVAL 30 DAY, SYSDATE() + INTERVAL 30000 DAY, SYSDATE(), null);
